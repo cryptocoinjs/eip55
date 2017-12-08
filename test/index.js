2e166091608f8fe66a0af0e9e1767cf3f303f048
Daniel Cousens <github@dcousens.com>
2017-12-08 14:02:06 +1100

    tests: add random reference implementation tests

--- test/index.js
+++ test/index.js
@@ -10,6 +10,25 @@ function malform (address) {
   return address.slice(0, i) + s + address.slice(i + 1)
 }
 
+// from https://github.com/ethereum/EIPs/blob/f3a591f6718035ba358d6a479cadabe313f6ed36/EIPS/eip-55.md#implementation
+var createKeccakHash = require('keccak')
+var crypto = require('crypto')
+function referenceImpl (address) {
+  address = address.toLowerCase().replace('0x', '')
+  var hash = createKeccakHash('keccak256').update(address).digest('hex')
+  var ret = '0x'
+
+  for (var i = 0; i < address.length; i++) {
+    if (parseInt(hash[i], 16) >= 8) {
+      ret += address[i].toUpperCase()
+    } else {
+      ret += address[i]
+    }
+  }
+
+  return ret
+}
+
 tape('verifies each address', function (t) {
   var fail = 0
 
@@ -25,6 +44,12 @@ tape('verifies each address', function (t) {
       fail++
     }
   })
+
+  for (var i = 0; i < 1000; ++i) {
+    var address = '0x' + crypto.randomBytes(20).toString('hex')
+
+    t.same(referenceImpl(address), eip55.encode(address))
+  }
 
   t.ok(fail < 3) // ~0.03% rate
   t.end()
